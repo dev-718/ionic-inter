518ed4f5b53e61e4f786b97abfc44a4c632c3beb
rajel <superdev718@gmail.com>
2025-03-31 11:24:49 -0700

    a fourth-question

--- src/app/fourth-question/fourth-question.component.ts
+++ src/app/fourth-question/fourth-question.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonButton, IonProgressBar } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-fourth-question',
   templateUrl: './fourth-question.component.html',
   styleUrls: ['./fourth-question.component.scss'],
+  imports: [IonProgressBar, IonButton]
 })
 export class FourthQuestionComponent  implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {}
+
+  public clickAnswer(): void {
+    this.router.navigate(['/fifth']);
+  }
 
 }
