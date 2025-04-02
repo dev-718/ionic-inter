4326495dcb12e762c894cb3925da897187aee9f8
rajel <superdev718@gmail.com>
2025-04-02 09:32:28 -0700

    u validation

--- src/app/pix-key-validation/pix-key-validation.component.ts
+++ src/app/pix-key-validation/pix-key-validation.component.ts
@@ -1,22 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import {MaskitoDirective} from '@maskito/angular';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { IonButton, IonInput } from "@ionic/angular/standalone";
+import { IonButton, IonInput } from '@ionic/angular/standalone';
+import {MaskitoOptions, MaskitoElementPredicate} from '@maskito/core';
 
 @Component({
   selector: 'app-pix-key-validation',
   templateUrl: './pix-key-validation.component.html',
   styleUrls: ['./pix-key-validation.component.scss'],
-  imports: [IonInput, IonButton]
+  imports: [IonInput, IonButton, CommonModule, ReactiveFormsModule, MaskitoDirective],
 })
-export class PixKeyValidationComponent  implements OnInit {
+export class PixKeyValidationComponent implements OnInit {
+  public validationFormGroup!: FormGroup;
+  public cpfMask: MaskitoOptions = {
+    mask: [/\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '-', /\d/, /\d/,]
+  };
+  public phoneMask: MaskitoOptions = {
+    mask: ['(', /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]
+  };
+  readonly maskPredicate: MaskitoElementPredicate = async element => (element as HTMLIonInputElement).getInputElement();
 
-  constructor(
-    private router: Router
-  ) { }
+  constructor(private router: Router, private fb: FormBuilder) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.validationFormGroup = this.fb.group({
+      type: ['cpf', [Validators.required]],
+      cpf: [null, [Validators.required, Validators.pattern(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)]],
+      phone: [null, [Validators.pattern(/^\(\d{2}\) \d{4}-\d{4}$/)]],
+    });
+  }
 
   public validate(): void {
     this.router.navigate(['receive-balance']);
+  }
+
+  public setType(val: 'cpf' | 'phone'): void {
+    this.validationFormGroup.controls['type'].setValue(val);
+    if (val === 'cpf') {
+      this.validationFormGroup.controls['cpf'].addValidators([Validators.required]);
+      this.validationFormGroup.controls['phone'].removeValidators([Validators.required]);
+    } else {
+      this.validationFormGroup.controls['cpf'].removeValidators([Validators.required]);
+      this.validationFormGroup.controls['phone'].addValidators([Validators.required]);
+    }
+    this.validationFormGroup.controls['cpf'].updateValueAndValidity();
+    this.validationFormGroup.controls['phone'].updateValueAndValidity();
   }
 }
--- src/app/validation/validation.component.ts
+++ src/app/validation/validation.component.ts
@@ -3,17 +3,33 @@ import { IonButton, IonInput } from '@ionic/angular/standalone';
 import { ModalController } from '@ionic/angular';
 import { CongratulationsModalComponent } from '../congratulations-modal/congratulations-modal.component';
 import { LoadingComponent } from '../loading/loading.component';
+import {MaskitoOptions, MaskitoElementPredicate} from '@maskito/core';
+import {MaskitoDirective} from '@maskito/angular';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-validation',
   templateUrl: './validation.component.html',
   styleUrls: ['./validation.component.scss'],
-  imports: [IonInput, IonButton],
+  imports: [IonInput, IonButton, MaskitoDirective, ReactiveFormsModule],
 })
 export class ValidationComponent implements OnInit {
-  constructor(private modalCtrl: ModalController) {}
+  readonly maskPredicate: MaskitoElementPredicate = async element => (element as HTMLIonInputElement).getInputElement();
+  public cpfMask: MaskitoOptions = {
+    mask: [/\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '-', /\d/, /\d/,]
+  };
+  public cpfFormGroup!: FormGroup;
 
-  ngOnInit() {}
+  constructor(
+    private modalCtrl: ModalController,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.cpfFormGroup = this.fb.group({
+      cpf: [null, [Validators.required, Validators.pattern(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)]]
+    })
+  }
 
   public async seeValidation(): Promise<void> {
     this.modalCtrl.dismiss();
