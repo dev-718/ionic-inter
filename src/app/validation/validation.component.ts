1edf84be5a32b3e4327bb56d052af34e0d8c79a9
rajel <superdev718@gmail.com>
2025-04-01 18:13:57 -0700

    a loading

--- src/app/validation/validation.component.ts
+++ src/app/validation/validation.component.ts
@@ -1,28 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { IonButton, IonInput } from "@ionic/angular/standalone";
+import { IonButton, IonInput } from '@ionic/angular/standalone';
 import { ModalController } from '@ionic/angular';
 import { CongratulationsModalComponent } from '../congratulations-modal/congratulations-modal.component';
+import { LoadingComponent } from '../loading/loading.component';
 
 @Component({
   selector: 'app-validation',
   templateUrl: './validation.component.html',
   styleUrls: ['./validation.component.scss'],
-  imports: [IonInput, IonButton]
+  imports: [IonInput, IonButton],
 })
-export class ValidationComponent  implements OnInit {
-
-  constructor(
-    private modalCtrl: ModalController
-  ) { }
+export class ValidationComponent implements OnInit {
+  constructor(private modalCtrl: ModalController) {}
 
   ngOnInit() {}
 
   public async seeValidation(): Promise<void> {
     this.modalCtrl.dismiss();
-    const modal = await this.modalCtrl.create({
-      component: CongratulationsModalComponent
+    const loadingModal = await this.modalCtrl.create({
+      component: LoadingComponent,
     });
-    modal.present();
+    loadingModal.present();
+    const congratulationsModal = await this.modalCtrl.create({
+      component: CongratulationsModalComponent,
+    });
+    setTimeout(() => {
+      loadingModal.dismiss();
+      congratulationsModal.present();
+    }, 8000);
   }
-
 }
